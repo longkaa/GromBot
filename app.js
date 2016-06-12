51c5093d8508c073c724b279ccbfc0aa020386d8
Anton Kirillov <aKirillo@microsoft.com>
2016-06-11 23:54:32 -0700

    fixed app.js

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+// Get secrets from server environment
+var botConnectorOptions = { 
+    appId: process.env.BOTFRAMEWORK_APPID, 
+    appSecret: process.env.BOTFRAMEWORK_APPSECRET 
+};
+
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({appid: 'a777', appSecret: '42'});
+var bot = new builder.BotConnectorBot(botConnectorOptions);
 bot.add('/', function (session) {
-    session.send('Whuff, Whuff !!!');    
+    session.send('Whoof, Whoof !!!');    
 })
 
 // Setup Restify Server
 var server = restify.createServer();
 server.post('api/messages', bot.verifyBotFramework(), bot.listen());
+
+// Serve a static web page
+server.get(/.*/, restify.serveStatic({
+	'directory': '.',
+	'default': 'index.html'
+}));
+
 server.listen(process.env.p || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
-})
+});
